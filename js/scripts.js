39cd83b5741d0788f8b973b23b3f8261ef197abd
nimkus <164895942+nimkus@users.noreply.github.com>
2024-04-14 16:20:02 +0200

    setup pokemonList Array

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+let name = null;
+let height = null;
+let types = null;
+
+let pokemonList = [
+    { name: Bulbasaur, height: 0.7, types: ['grass', 'poison'] },
+    { name: Charmander, height: 0.6, types: ['fire'] },
+    { name: Squirtle, height: 0.5, types: ['water'] }
+];
+
+document.write(pokemonList[0]);
