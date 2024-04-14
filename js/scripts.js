eedea0c14f010b645ddbd0ea7bec6b161bc59a5b
nimkus <164895942+nimkus@users.noreply.github.com>
2024-04-14 16:51:24 +0200

    fixed issues with pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,9 @@
-let name = null;
-let height = null;
-let types = null;
+ /* stylelint-disable */ 
 
 let pokemonList = [
-    { name: Bulbasaur, height: 0.7, types: ['grass', 'poison'] },
-    { name: Charmander, height: 0.6, types: ['fire'] },
-    { name: Squirtle, height: 0.5, types: ['water'] }
+    { name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison'] },
+    { name: 'Charmander', height: 0.6, type: ['fire'] },
+    { name: 'Squirtle', height: 0.5, type: ['water'] }
 ];
 
-document.write(pokemonList[0]);
+document.write(pokemonList[0].name);
