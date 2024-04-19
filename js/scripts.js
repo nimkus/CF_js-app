151980e1e5f51f00859f0c8a87316f8c98c051e8
nimkus <164895942+nimkus@users.noreply.github.com>
2024-04-19 11:31:07 +0200

    added a conditional loop to the Pokedex

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,29 @@
- /* stylelint-disable */ 
+"Use strict"
 
 let pokemonList = [
-    { name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison'] },
-    { name: 'Charmander', height: 0.6, type: ['fire'] },
-    { name: 'Squirtle', height: 0.5, type: ['water'] }
+    { 
+      name: 'Bulbasaur', 
+      height: 0.7, 
+      type: ['grass', 'poison'] 
+    },
+    { 
+      name: 'Charmander', 
+      height: 0.6, 
+      type: ['fire'] 
+    },
+    { 
+      name: 'Squirtle',
+      height: 0.5, 
+      type: ['water'] 
+    }
 ];
 
-document.write(pokemonList[0].name);
+for (let i = 0; i < pokemonList.length; i++) {
+  document.write('<p>' + pokemonList[i].name + ' has a height of ' + pokemonList[i].height + 'm.');
+  if (pokemonList[i].height >= 0.7) {
+    document.write(' ---> Wow, that’s big!</p>');
+  }
+  else {
+    document.write('</p>');
+  }
+}
